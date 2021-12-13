a9ab38c68fdecf6264eb8a2144eef1d087473922
Mikael Larsson <mikael.larsson@epiroc.com>
2021-12-13 21:37:31 +0100

    Format all lua code

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,70 +1,78 @@
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
 
     -- Mappings.
-    local opts = { noremap=true, silent=true }
+    local opts = {noremap = true, silent = true}
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', '<leader>f',        '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', "<leader>fd",       "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>", {silent=true, noremap=true})
-    buf_set_keymap('n', '<leader>fh',       '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', "<leader>fi",       "<cmd>lua require('telescope.builtin').lsp_implementations()<CR>", {silent=true, noremap=true})
-    buf_set_keymap('n', "<leader>fr",       "<cmd>lua require('telescope.builtin').lsp_references()<CR>", {silent=true, noremap=true})
-    buf_set_keymap('n', '<leader>fx',       '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', '<localleader>fs',  '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<localleader>ft',  '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn',        '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>e',         '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    buf_set_keymap('n', '[d',               '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d',               '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', 'g=',               '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    buf_set_keymap('x', 'g=',               '<cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
-  
+    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.declaration()<CR>',
+                   opts)
+    buf_set_keymap('n', "<leader>fd",
+                   "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>",
+                   {silent = true, noremap = true})
+    buf_set_keymap('n', '<leader>fh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', "<leader>fi",
+                   "<cmd>lua require('telescope.builtin').lsp_implementations()<CR>",
+                   {silent = true, noremap = true})
+    buf_set_keymap('n', "<leader>fr",
+                   "<cmd>lua require('telescope.builtin').lsp_references()<CR>",
+                   {silent = true, noremap = true})
+    buf_set_keymap('n', '<leader>fx', '<cmd>lua vim.lsp.buf.code_action()<CR>',
+                   opts)
+    buf_set_keymap('n', '<localleader>fs',
+                   '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<localleader>ft',
+                   '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<space>e',
+                   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+                   opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
+                   opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
+                   opts)
+    buf_set_keymap('n', 'g=', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    buf_set_keymap('x', 'g=', '<cmd>lua vim.lsp.buf.range_formatting()<CR>',
+                   opts)
+
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local lsp = require'lspconfig'
+local lsp = require 'lspconfig'
 
 if vim.fn.executable('clangd') > 0 then
     lsp.clangd.setup {
         on_attach = on_attach,
         capabilities = capabilities,
         cmd = {
-            "clangd",
-            "--background-index=true",
-            "--clang-tidy=true",
-            "--completion-style=detailed",
-            "--all-scopes-completion=true"
-        },
+            "clangd", "--background-index=true", "--clang-tidy=true",
+            "--completion-style=detailed", "--all-scopes-completion=true"
+        }
     }
 end
 
- if vim.fn.executable('rls') > 0 then
-    lsp.rls.setup{
+if vim.fn.executable('rls') > 0 then
+    lsp.rls.setup {
         on_attach = on_attach,
         capabilities = capabilities,
         settings = {
             rust = {
                 unstable_features = true,
                 build_on_save = false,
-                all_features = true,
+                all_features = true
             }
-        },
+        }
     }
 end
 
 if vim.fn.executable('rust_analyzer') > 0 then
-    lsp.rust_analyzer.setup{
-        on_attach = on_attach,
-        capabilities = capabilities,
-    }
+    lsp.rust_analyzer.setup {on_attach = on_attach, capabilities = capabilities}
 end
 
 if vim.fn.executable('pyright') > 0 then
-    lsp.pyright.setup{
-        on_attach = on_attach,
-        capabilities = capabilities,
-    }
+    lsp.pyright.setup {on_attach = on_attach, capabilities = capabilities}
 end
--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,14 +1,12 @@
 require'nvim-treesitter.configs'.setup {
     incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "i",
-        scope_incremental = "+",
-        node_decremental = "-",
-      },
-    },
-    highlight = {
         enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "i",
+            scope_incremental = "+",
+            node_decremental = "-"
+        }
     },
-  }
+    highlight = {enable = true}
+}
