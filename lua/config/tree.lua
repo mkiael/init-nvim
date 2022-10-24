20d18deac184f29373c70598f0c50a827bbb0f5b
Mikael Larsson <mikael.larsson@epiroc.com>
2022-10-24 09:09:04 +0200

    Make nvim tree size adaptive

--- lua/config/tree.lua
+++ lua/config/tree.lua
@@ -2,4 +2,18 @@ vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle<CR>', {noremap = true
 vim.api.nvim_set_keymap('n', '<F2>', ':NvimTreeFindFile<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<F5>', ':NvimTreeRefresh<CR>', {noremap = true})
 
-require'nvim-tree'.setup{}
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require'nvim-tree'.setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
